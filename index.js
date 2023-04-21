6cf91df26b949ff84938f3529958d77cd8c266cd
lhc <62022074+lhc19890816@users.noreply.github.com>
2023-04-21 10:26:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function SayHello(){
-    //console.log("Hello World....")
-    return "Hello World..."
-}
+const http = require('http');
 
-module.exports=SayHello
+const hostname = '0.0.0.0';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
